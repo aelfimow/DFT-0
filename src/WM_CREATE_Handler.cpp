635664f569f50e99a161186d553aa6fb448e9aca
Alexander Elfimow <my.free.mail@gmx.de>
2018-05-17 21:33:28 +0200

    Added File Open dialog

--- src/WM_CREATE_Handler.cpp
+++ src/WM_CREATE_Handler.cpp
@@ -15,5 +15,32 @@ WM_CREATE_Handler::~WM_CREATE_Handler()
 
 LRESULT WM_CREATE_Handler::operator()([[maybe_unused]] HWND hwnd, [[maybe_unused]] WPARAM wParam, [[maybe_unused]] LPARAM lParam)
 {
+    TCHAR szFileName[MAX_PATH];
+    ::ZeroMemory(szFileName, sizeof(szFileName));
+
+    TCHAR szFileTitle[MAX_PATH];
+    ::ZeroMemory(szFileTitle, sizeof(szFileTitle));
+
+    OPENFILENAME ofn;
+    ::ZeroMemory(&ofn, sizeof(ofn));
+
+    ofn.hwndOwner      = hwnd;
+    ofn.lpstrFilter    = TEXT("*");
+
+    ofn.lpstrFile      = szFileName;
+    ofn.nMaxFile       = MAX_PATH;
+
+    ofn.lpstrFileTitle = szFileTitle;
+    ofn.nMaxFileTitle  = MAX_PATH;
+
+    ofn.lStructSize    = sizeof(ofn);
+
+    auto result = ::GetOpenFileName(&ofn);
+
+    if (result != 0)
+    {
+        // ofn.lpstrFile and ofn.lpstrFileTitle are valid
+    }
+
     return 0;
 }
